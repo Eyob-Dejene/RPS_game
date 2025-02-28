208051dfcf1dbf1d1e09c04341cec9407002b104
Eyob-D <eyobdejenee@gmail.com>
2025-02-28 01:12:04 +0100

    Full game logic implemented with 3 round plays

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let humanScore =0;
 let computerScore =0;
 let compChoice;
+let humanWon ="Human Won";
+let compWon="Computer Won";
 
+let winnerPlayer;
+let humanHighscore =0;
+let compHighScore =0;
 function getComputerChoice(compChoice){
     compChoice = Math.floor(Math.random() * 3);
     //console.log(compChoice);
@@ -35,26 +40,79 @@ function getHumanChoice (humChoice){
 
 function playRound (humChoice,compChoice ){
 
+
     if (humChoice === compChoice){
-        console.log("Tie! Play Again!")
+        humanScore=0;
+        computerScore=0;
     } else if ((humChoice === "ROCK") && (compChoice === "PAPER")){
         console.log("You Lost! "+ compChoice + " beats " + humChoice)
-    } else if ((humChoice == "ROCK") && (compChoice == "SISSORS")) {
+        computerScore++;
+    } else if ((humChoice === "ROCK") && (compChoice === "SISSORS")) {
         console.log("You Won! "+ humChoice + " beats " + compChoice)
-    } else if ((humChoice == "PAPER") && (compChoice == "ROCK" )){
+        humanScore ++
+    } else if ((humChoice === "PAPER") && (compChoice === "ROCK" )){
         console.log("You Won! "+ humChoice + " beats " + compChoice)
-    } else if ((humChoice == "PAPER") && (compChoice == "SISSORS")){
+        humanScore++;
+    } else if ((humChoice === "PAPER") && (compChoice === "SISSORS")){
         console.log("You Lost! "+ compChoice + " beats " + humChoice)
-    } else if ((humChoice == "SISSORS") && (compChoice == "ROCK")){
+        computerScore++;
+    } else if ((humChoice === "SISSORS") && (compChoice === "ROCK")){
         console.log("You Lost! "+ compChoice + " beats " + humChoice)
-    } else if ((humChoice == "SISSORS") && (compChoice == "PAPER")){
+        computerScore++;
+    } else if ((humChoice === "SISSORS") && (compChoice === "PAPER")){
         console.log("You Won! "+ humChoice + " beats " + compChoice)
+        humanScore++;
 
     } else {
         console.log("Unknown Result")
 
     }
+    if (humanScore === computerScore){
+     return 0;
+    } else if (humanScore > computerScore){
+        return humanWon;
+
+    } else if (computerScore > humanHighscore){
+        return compWon;
+    }
+    else 
+     return 0;
 }
-playRound(getHumanChoice(),getComputerChoice());
+//console.log(playRound(getHumanChoice(),getComputerChoice()));
+
+
+function playGame(rounds){
+  
+    winnerPlayer=playRound(getHumanChoice(),getComputerChoice());
+if (winnerPlayer === "Human Won"){
+ humanHighscore ++;
+} else if (winnerPlayer === "Computer Won"){
+    compHighScore ++;
+} else if (winnerPlayer === 0){
+    console.log("Tie! No Points for both")
+} else {
+    console.log("Something is Wrong!")
+}
+
+}
+playGame();
+playGame();
+playGame();
+playRounds();
+function playRounds(){
+if (humanHighscore > compHighScore){
+    console.log("You WON The Game !!")
+} else if (compHighScore > humanHighscore){
+    console.log("Computer WON The Game !!")
+} else if (compHighScore === humanHighscore){
+    alert("It's a TIE! Play Again :-) ");
+}
+
+else {
+    alert("No Winner Play one More round.")
+}
+}
+
+
 
 
